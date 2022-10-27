e704ed759d1919f03e82a6e9e637225f5df230f3
Paul Walsh <paul@walsh.co.il>
2022-10-27 19:43:04 +0300

    update template

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-contactButton = document.querySelector("#contact button");
+//////////////
+// we have a basic skeleton here to help you start.
+// if you dont want to use it you dont have to -
+// just clear the file and start from scratch
+//////////////
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
-});
+// notice in our html we have a node with ID "app"
+// hint: use this reference later to inject data into your page
+const app = document.getElementById('app');
+
+async function getData() {
+  // write you logic for getting the data from the API here
+  // return your data from this function
+}
+
+async function render() {
+  data = await getData();
+  // you have your data! add logic here to render it to the UI
+  // notice in the HTML file we call render();
+}
