3b1f0fe4583ccc3dc9db3ba9f0ccae2bfd26b400
Paul Walsh <paul@walsh.co.il>
2022-11-08 17:41:57 +0200

    update template

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ const app = document.getElementById('app');
 async function getData() {
   // write you logic for getting the data from the API here
   // return your data from this function
+  const data = null;
+  return data;
 }
 
-async function render() {
-  data = await getData();
+function clearUI() {
+  while (app.firstChild) {
+    app.removeChild(app.firstChild);
+  }
+}
+
+async function renderUI(data) {
+
+  clearUI();
+
   // you have your data! add logic here to render it to the UI
   // notice in the HTML file we call render();
+  const dummyItemElement = Object.assign(document.createElement("div"), { className: "item" })
+  const dummyContentElement = Object.assign(document.createElement("div"), { className: "content" })
+  dummyContentElement.innerHTML = "hey";
+  dummyItemElement.appendChild(dummyContentElement);
+  app.appendChild(dummyItemElement);
 }
+
+const data = await getData();
+
+await renderUI(data);
